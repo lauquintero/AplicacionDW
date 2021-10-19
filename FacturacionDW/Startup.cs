62318ac9000b0127453db9d8bee1be9c584c4470
Laureano Quintero Salcedo <ingeniero.master2@interrapidisimo.com>
2021-10-19 17:44:45 -0500

    cambios corrección swagger

--- FacturacionDW/Startup.cs
+++ FacturacionDW/Startup.cs
@@ -2,19 +2,13 @@ using Facturacion.Singleton;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FacturacionDW
 {
@@ -67,7 +61,9 @@ namespace FacturacionDW
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FacturacionDW v1"));
+                app.UseSwaggerUI(c => { var swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                    c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "FacturacionDW v1");
+                } );
             }
 
             app.UseHttpsRedirection();
